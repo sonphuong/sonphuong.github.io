63bcc0dd2d797625f5063f536e8a81e553ca9ca8
sonphuong <psd@easytranslate.com>
2020-04-24 23:04:57 +0700

    first

---
+++ express/server.js
@@ -0,0 +1,16 @@
+'use strict';
+//to know more about 'use strict' https://www.w3schools.com/js/js_strict.asp
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
